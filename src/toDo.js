91cb1fa568c38018ad37a1299e14de9ddb0dffae
Nedim <nedim_mulahusic@yahoo.com>
2026-01-23 11:02:40 -0700

    created a todo class where i can create a todo item tyo add into my project index

---
+++ src/toDo.js
@@ -0,0 +1,8 @@
+export class ToDo{
+    //create a constructor, for now i weill leve off the notes and check list
+    constructor(title, description, dueDate){
+        this._title = title;
+        this._description = description;
+        this._dueDate = dueDate;
+    }
+}
